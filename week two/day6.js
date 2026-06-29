7983a127c78b02ac47b0f6a3ec16607a7a03d218
Esha <erehmana786@gmail.com>
2026-06-29 13:05:27 +0500

    notes

--- week two/day6.js
+++ week two/day6.js
@@ -309,17 +309,18 @@
 // false == "" // true 
 // 0==""  // true 
 
-// // Nullish coalescing operator (??) : null undefined 
-// let val1;
-// val1 = 3??5
-// // val1 = null ?? 5
-// console.log(val1);
+// Nullish coalescing operator (??) : null undefined 
+let val1;
+val1 = 3??5
+// val1 = null ?? 5
+val1 = undefined ?? 5
+console.log(val1);
 
-let score = 0;
-// OR (||) operator treats 0 as a falsy value, so it falls back to 10
-let result1 = score || 10; // Result: 10
-console.log(result1);
-// Nullish Coalescing (??) operator only checks for null/undefined. 
-// Since 0 is a valid number, it keeps 0.
-let result2 = score ?? 10; // Result: 0
-console.log(result2);
+// let score = 0;
+// // OR (||) operator treats 0 as a falsy value, so it falls back to 10
+// let result1 = score || 10; // Result: 10
+// console.log(result1);
+// // Nullish Coalescing (??) operator only checks for null/undefined. 
+// // Since 0 is a valid number, it keeps 0.
+// let result2 = score ?? 10; // Result: 0
+// console.log(result2);
