b17fe9598f396e75257da22524c5e589153b5613
Esha <erehmana786@gmail.com>
2026-09-04 17:12:39 +0500

    DOM element

--- week two/DOMpractice.js
+++ week two/DOMpractice.js
@@ -1,3 +1,11 @@
 const parent = document.querySelector('.Parent')
-console.log(parent);
+// console.log(parent);
 // console.log(parent.children);
+// console.log(parent.children[3].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+    console.log(parent.children[i].innerHTML);
+    
+}
+
+parent.children[1].style.color = "red"
