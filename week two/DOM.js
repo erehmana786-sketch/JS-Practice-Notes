43bb83ab715bfb5d52c1bfeb687777b7fbd44de5
Esha <erehmana786@gmail.com>
2026-09-21 19:35:57 +0500

    DOM practice

--- week two/DOM.js
+++ week two/DOM.js
@@ -21,6 +21,3 @@ paragraph.innerHTML = "Hey there! <b>Esha here</b>" // Hey there! Esha here (Esh
 
 // create list via innerHTML
 paragraph.innerHTML="<ul><li>HTML</li><li>CSS</li><li>JavaScript</li></ul>"
-
- myProfile = {
-  name: "Esha"
