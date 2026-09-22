a1b768e74f7ee04dbdcc99d69cb2371b43ed7561
Esha <erehmana786@gmail.com>
2026-09-22 20:36:53 +0500

    DOM practice

--- week two/DOM.js
+++ week two/DOM.js
@@ -21,12 +21,3 @@ paragraph.innerHTML = "Hey there! <b>Esha here</b>" // Hey there! Esha here (Esh
 
 // create list via innerHTML
 paragraph.innerHTML="<ul><li>HTML</li><li>CSS</li><li>JavaScript</li></ul>"
-
-// Naya paragraph banao:
-var newPara = document.createElement("p");
-newPara.innerText    = "JS ne mujhe banaya!";
-newPara.style.color  = "purple";
-newPara.style.fontSize = "18px";
-document.body.appendChild(newPara);
-
-// Array se list banana — REACT KA BASE!
