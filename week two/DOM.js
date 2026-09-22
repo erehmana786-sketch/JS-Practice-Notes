ad51eb20d32926f08bb979ba701aa0b70e8d06fe
Esha <erehmana786@gmail.com>
2026-09-22 20:38:09 +0500

    DOM practice

--- week two/DOM.js
+++ week two/DOM.js
@@ -21,3 +21,10 @@ paragraph.innerHTML = "Hey there! <b>Esha here</b>" // Hey there! Esha here (Esh
 
 // create list via innerHTML
 paragraph.innerHTML="<ul><li>HTML</li><li>CSS</li><li>JavaScript</li></ul>"
+
+
+var newPara = document.createElement("p");
+newPara.innerText    = "JS ne mujhe banaya!";
+newPara.style.color  = "purple";
+newPara.style.fontSize = "18px";
+document.body.appendChild(newPara);
